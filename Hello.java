f2aa57ec0d953c686740060a4a4acfdf3d6188c4
bharath_varala <bharathvarala11@gmail.com>
2024-08-01 12:24:00 +0530

    core java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,40 @@
-class prac{
+class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello");
+        // print statement
+        // System.out.println("Hello");
+        
+        // variables
+        // int num1 = 10;
+        // int num2 = 20;
+        // int res = num1+num2;
+        // System.out.println(res);
+        
+        // data types
+        // 2 types of data types 
+        // 1. primitive
+        //     integer
+        //     float 
+        //     character
+        //     boolean 
+        // 2. derived
+
+        // float val = 6.27f;
+        // int num = 10;
+        // System.out.println(num+val);
+
+        // type coversion
+        // int a = 257;
+        // byte b;
+        // b = (byte)a;
+        // System.out.println(b);
+
+        // arithmetic operators
+
+
+
+
+
+
+
     }
 }
