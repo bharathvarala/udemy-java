39bf7d058eee9872b4fbc7b1693fde8f77e45fea
bharath_varala <bharathvarala11@gmail.com>
2024-08-22 18:30:33 +0530

    java

--- Hello.java
+++ Hello.java
@@ -47,9 +47,47 @@ class Hello{
         // Relational operators -- == > < >= <= !=
         // Logical operators -- && || !
 
-        
+        // conditional statements
+        // if else
+        // int n=10;
+        // if(n<=10){
+        //     System.out.println("Yes");
+        // }
+        // else{
+        //     System.out.println("No");
+        // }
 
+        // if else if
+        // int a=15;
+        // if(a<=12){
+        //     System.out.println("You are a child");
+        // }
+        // else if(a>10 && a<18){
+        //     System.out.println("You are close");
+        // }
+        // else{
+        //     System.out.println("You are an adult");
+        // }
 
+        // switch case
+        // int n=30;
+        // switch (n) {
+        //     case 10:
+        //         System.out.println(10);
+        //         break;
+        //     case 20:
+        //         System.out.println(20);
+        //         break;
+        //     default:
+        //     System.out.println("Not matching");
+        //         break;
+        // }
+
+        // ternary operator
+        // int res = 10;
+        // int n = -1;
+        // res = (n%2==0)?1:0;
+        // System.out.println(res);
 
 
 
